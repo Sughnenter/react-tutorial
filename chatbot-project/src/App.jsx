3e081892df9b2bc0b1cb604ed65916dee8ebe02d
shammah <shamaj2006@gmail.com>
2025-12-15 18:13:26 +0100

    moved chatbot to react created a component folder and moved components there

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,56 +1,13 @@
-import { useState, useRef, useEffect  } from 'react'
-import {Chatbot} from 'supersimpledev'
-import './App.css'
-
-
-      function ChatInput({ chatMessages, setChatMessages }) {
-        const [inputText, setInputText] = useState("");
-        function saveInputText(event) {
-          setInputText(event.target.value);
-        }
-        function sendMessage() {
-          const newChatMessages = [
-            ...chatMessages,
-            {
-              message: inputText,
-              sender: "user",
-              id: crypto.randomUUID(),
-            },
-          ];
-          setChatMessages(newChatMessages);
-
-          const response = Chatbot.getResponse(inputText);
-          setChatMessages([
-            ...newChatMessages,
-            {
-              message: response,
-              sender: "robot",
-              id: crypto.randomUUID(),
-            },
-          ]);
-
-          setInputText("");
-        }
-        return (
-          <div className="chat-input-container">
-            <input
-              placeholder="Send a message to chatbot"
-              size="30"
-              onChange={saveInputText}
-              value={inputText}
-              className="chat-input"
-            />
-            <button onClick={sendMessage} className="send-button">
-              Send
-            </button>
-          </div>
-        );
-      }
-      function ChatMessage({ message, sender }) {
-        //const message = props.message;
-        //const sender = props.sender;
-        //const {message, sender} = props;
-        /*
+import { useState, useRef, useEffect } from "react";
+import { ChatInput } from "./components/ChatInput";
+import RobotProfileImage from "./assets/robot.png";
+import UserProfileImage from "./assets/user.png";
+import "./App.css";
+function ChatMessage({ message, sender }) {
+  //const message = props.message;
+  //const sender = props.sender;
+  //const {message, sender} = props;
+  /*
         if(sender === 'robot'){
           return(
             <div>
@@ -61,73 +18,65 @@ import './App.css'
         }
           */
 
+  return (
+    <div
+      className={sender === "user" ? "chat-message-user" : "chat-message-robot"}
+    >
+      {sender === "robot" && (
+        <img src={RobotProfileImage} className="chat-message-profile" />
+      )}
+      <div className="chat-message-text">{message}</div>
+      {sender === "user" && (
+        <img src={UserProfileImage} className="chat-message-profile" />
+      )}
+    </div>
+  );
+}
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMesssage) => {
         return (
-          <div
-            className={
-              sender === 'user'
-              ? 'chat-message-user' 
-              : 'chat-message-robot'
-            }
-          >
-            {sender === "robot" && (
-              <img src="robot.png" 
-              className="chat-message-profile" />
-            )}
-            <div className="chat-message-text">{message}</div>
-            {sender === "user" && (
-              <img src="user.png" 
-              className="chat-message-profile" />
-            )}
-          </div>
+          <ChatMessage
+            message={chatMesssage.message}
+            sender={chatMesssage.sender}
+            key={chatMesssage.id}
+          />
         );
-      }
-
-      function ChatMessages({ chatMessages }) {
-        const chatMessagesRef = useRef(null);
-
-        useEffect(()=>{
-          const containerElem = chatMessagesRef.current
-          if(containerElem){
-            containerElem.scrollTop = containerElem.scrollHeight;
-          }
-        }, [chatMessages]);
-        return (
-          <div className="chat-messages-container" ref={chatMessagesRef}>
-            {chatMessages.map((chatMesssage) => {
-              return (
-                <ChatMessage
-                  message={chatMesssage.message}
-                  sender={chatMesssage.sender}
-                  key={chatMesssage.id}
-                />
-              );
-            })}
-          </div>
-        );
-      }
-
-
+      })}
+    </div>
+  );
+}
 
 function App() {
-        const [chatMessages, setChatMessages] = useState([
-          { message: "Hello Chatbot", sender: "user", id: "id1" },
-          { message: "Hello! How can I help you?", sender: "robot", id: "id2" },
-          { message: "Can you get me todays date?", sender: "user", id: "id3" },
-          { message: "Today is september 27", sender: "robot", id: "id4" },
-        ]);
-        //const [chatMessages, setChatMessages] = array;
-        //const chatMessages = array[0];
-        //const setChatMessages = array[1];
+  const [chatMessages, setChatMessages] = useState([
+    { message: "Hello Chatbot", sender: "user", id: "id1" },
+    { message: "Hello! How can I help you?", sender: "robot", id: "id2" },
+    { message: "Can you get me todays date?", sender: "user", id: "id3" },
+    { message: "Today is september 27", sender: "robot", id: "id4" },
+  ]);
+  //const [chatMessages, setChatMessages] = array;
+  //const chatMessages = array[0];
+  //const setChatMessages = array[1];
 
-        return (
-          <div className="app-container">
-            <ChatMessages chatMessages={chatMessages} />
-            <ChatInput
-              chatMessages={chatMessages}
-              setChatMessages={setChatMessages}
-            />
-          </div>
-        );
-      }
+  return (
+    <div className="app-container">
+      <ChatMessages chatMessages={chatMessages} />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  );
+}
 
-export default App
+export default App;
