cab97b5eb9be85683850b257840ba0fe144aa2c0
shammah <shamaj2006@gmail.com>
2025-12-15 18:39:50 +0100

    moved css to components folder and seperate css files and imported them in their respective components

--- chatbot-project/src/components/ChatMessages.jsx
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -1,5 +1,7 @@
 import {useRef, useEffect} from 'react'
 import { ChatMessage } from './ChatMessage'
+import './ChatMessages.css';
+
 function ChatMessages({ chatMessages }) {
   const chatMessagesRef = useRef(null);
 
