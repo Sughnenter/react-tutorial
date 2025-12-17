56eb630537b7500754b6100a3160f402d568242c
shammah <shamaj2006@gmail.com>
2025-12-17 19:39:54 +0100

    installed axios and used it to fetch data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,13 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
-  fetch("http://localhost:3000/api/products")
+  axios.get("http://localhost:3000/api/products")
     .then((response) => {
-      return response.json();
+      confirm.log(response.data);
     })
-    .then((data) => {
-      console.log(data);
-    });
   //.then creates a promise that resolves to the response of the request
   return (
     <>
