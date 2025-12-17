479f54fc23c948aa5a36462c6d75588d5422d8de
shammah <shamaj2006@gmail.com>
2025-12-17 14:15:38 +0100

    6j moved images to assetes and imported them in jsx

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header";
-import {Link} from 'react-router';
+import BuyAgainIcon from "../assets/images/icons/buy-again.png";
+import { Link } from "react-router";
 
 import "./OrdersPage.css";
 export function OrdersPage() {
@@ -46,10 +47,7 @@ export function OrdersPage() {
                 </div>
                 <div className="product-quantity">Quantity: 1</div>
                 <button className="buy-again-button button-primary">
-                  <img
-                    className="buy-again-icon"
-                    src="images/icons/buy-again.png"
-                  />
+                  <img className="buy-again-icon" src={BuyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -75,10 +73,7 @@ export function OrdersPage() {
                 </div>
                 <div className="product-quantity">Quantity: 2</div>
                 <button className="buy-again-button button-primary">
-                  <img
-                    className="buy-again-icon"
-                    src="images/icons/buy-again.png"
-                  />
+                  <img className="buy-again-icon" src={BuyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -124,10 +119,7 @@ export function OrdersPage() {
                 </div>
                 <div className="product-quantity">Quantity: 2</div>
                 <button className="buy-again-button button-primary">
-                  <img
-                    className="buy-again-icon"
-                    src="images/icons/buy-again.png"
-                  />
+                  <img className="buy-again-icon" src={BuyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
