b5164c6439fce6a017fcec5e9f925dc144b1b046
shammah <shamaj2006@gmail.com>
2025-12-18 21:44:08 +0100

    excercise 7 i-o tracking page, and added post

--- ecommerce-project/src/pages/NotFound.jsx
+++ ecommerce-project/src/pages/NotFound.jsx
@@ -1,10 +1,10 @@
 import { Header } from "../components/header";
 import "./NotFound.css";
-export function NotFound() {
+export function NotFound({cart}) {
   return (
     <>
       <title>Page Not Found</title>
-      <Header />
+      <Header cart={cart} />
       <div className="not-found"> error 404 not found </div>
     </>
   );
